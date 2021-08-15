4b82a4c3b0834aaa2722bed23b7c6a800760aef0
Isadora Cristina <isadorac2008@hotmail.com>
2021-08-15 11:11:20 -0300

    Finished staging

---
+++ models/staging/stg_countryregion.sql
@@ -0,0 +1,16 @@
+with
+    source as (
+        select
+            countryregioncode,
+            name,
+            modifieddate,
+
+            rowguid,
+            _sdc_sequence,
+            _sdc_table_version,
+            _sdc_received_at,
+            _sdc_batched_at
+        from {{ source('adventureworks', 'countryregion') }}
+    )
+
+select * from source
